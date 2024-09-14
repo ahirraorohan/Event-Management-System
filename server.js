73af129e6ff847108e1e8580b84e404a546f8395
ahirraorohan <150143780+ahirraorohan@users.noreply.github.com>
2024-09-14 17:19:54 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/events', require('./routes/events'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
